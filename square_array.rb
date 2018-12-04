4164b289e5c3f49362f0c5d396b21ac5d2d67593
Evan Pavley <epavley@sva.edu>
2018-12-04 08:50:03 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  new_num_arr = []
+  numbers.each do |n|
+    new_num = n**2
+    new_num_arr.push(new_num)
+  end
+  return new_num_arr
 end
