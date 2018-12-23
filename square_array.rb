6312bb5d8112023320849272490c01b03bbe8e97
diaaanek <dkorongy@gmail.com>
2018-12-23 12:02:59 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+
+# call on collect/map or inject
+  # #square_array calls on each
+  squared_num = []
+  array.each { |x| squared_num << x **2 }
+  squared_num
+
+  # #square_array should square the elements in an array
 end
